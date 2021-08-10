73ac1c3e7e83651da740c2e3ade960b74e00b9cb
Sergey Kvachonok <ravenexp@gmail.com>
2021-08-10 16:27:59 +0300

    Add command line arguments parsing
    Implement command line arguments parsing and validation
    via the standard argparse module.
    
    Include all required command line arguments for the main
    `smb-net-ads-join` utility script.
    
    Add basic execution logging using the standard logging module.

--- README.md
+++ README.md
@@ -3,3 +3,25 @@ Offline Windows AD domain join tool for Samba
 
 Implements a custom `smb-net-ads-join` utility, which performs the same
 operation as `net ads join` Samba command, but works completely offline.
+
+To join a Samba system to an AD domain it needs `SYSTEM`, `SECURITY` and `SAM`
+registry hive files from a Windows system that is already joined to
+the same domain.
+
+Usage
+-----
+
+```
+usage: smb-net-ads-join [-h] [-v] [-V] [-J] [-o FILE] DIR
+
+positional arguments:
+  DIR                   Windows registry hive files directory (e.g. '/Windows/System32/config')
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         print intermediate results and debug info
+  -V, --version         show program's version number and exit
+  -J, --json            generate 'secrets.json' file in place of 'secrets.tdb'
+  -o FILE, --output FILE
+                        generated 'secrets.tdb' file name and location
+```
--- src/smbjoin/cli.py
+++ src/smbjoin/cli.py
@@ -2,10 +2,95 @@
 Command line interface entry point functions
 """
 
+import argparse
+import logging
+import os
+import sys
+
+import smbjoin
+
+logger = logging.getLogger(__name__)
+
+
 def net_ads_join() -> None:
     """
     The main `smb-net-ads-join` script entry point
+
+    This entry point function only handles the command line arguments
+    parsing and validation. It also sets up logging for the whole package.
     """
+
+    parser = argparse.ArgumentParser(prog="smb-net-ads-join")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="print intermediate results and debug info",
+    )
+    parser.add_argument(
+        "-V", "--version", action="version", version=smbjoin.__version__
+    )
+    parser.add_argument(
+        "-J",
+        "--json",
+        action="store_true",
+        help="generate 'secrets.json' file in place of 'secrets.tdb'",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        metavar="FILE",
+        default="secrets.tdb",
+        help="generated 'secrets.tdb' file name and location",
+    )
+    parser.add_argument(
+        "DIR",
+        help="Windows registry hive files directory (e.g. '/Windows/System32/config')",
+    )
+
+    args = parser.parse_args()
+
+    loglevel = (
+        logging.DEBUG
+        if args.verbose > 1
+        else logging.INFO
+        if args.verbose == 1
+        else logging.WARNING
+    )
+
+    logging.basicConfig(stream=sys.stdout, level=loglevel)
+
+    logger.debug("Executing smb-net-ads-join with args: %s", args)
+    logger.info("Looking for Windows registry hive files in '%s'", args.DIR)
+
+    system_hive_path = os.path.join(args.DIR, "SYSTEM")
+    security_hive_path = os.path.join(args.DIR, "SECURITY")
+    sam_hive_path = os.path.join(args.DIR, "SAM")
+
+    try:
+        with open(system_hive_path, "rb"):
+            logger.info("Found SYSTEM hive file at '%s'", system_hive_path)
+    except OSError as err:
+        logger.critical("Probing SYSTEM hive file failed: %s", err)
+        print("error: SYSTEM hive file not found", file=sys.stderr)
+        sys.exit(401)
+
+    try:
+        with open(security_hive_path, "rb"):
+            logger.info("Found SECURITY hive file at '%s'", security_hive_path)
+    except OSError as err:
+        logger.critical("Probing SECURITY hive file failed: %s", err)
+        print("error: SECURITY hive file not found", file=sys.stderr)
+        sys.exit(402)
+
+    try:
+        with open(sam_hive_path, "rb"):
+            logger.info("Found SAM hive file at '%s'", sam_hive_path)
+    except OSError as err:
+        logger.critical("Probing SAM hive file failed: %s", err)
+        print("error: SAM hive file not found", file=sys.stderr)
+        sys.exit(403)
 
     # STUB
     print("Joined 'host' to realm 'domain.com'")
